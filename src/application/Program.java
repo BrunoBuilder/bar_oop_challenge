6297b2c1e85180d1059ed33032e7181d1552d70e
BrunoBuilder <brunofernandotadeu@hotmail.com>
2022-06-14 22:55:12 -0300

    first class Program - implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,24 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Hello, world!");
+		char sex;
+		int quantBeers, quantSoftDrinks, quantbarbecues;
+		
+		System.out.print("Sex: ");
+		sex = sc.next().charAt(0);
+		System.out.print("quantity of beers: ");
+		quantBeers = sc.nextInt();
+		System.out.print("quantity of soft drinks: ");
+		quantSoftDrinks = sc.nextInt();
+		System.out.print("quantity of skewers: ");
+		quantbarbecues = sc.nextInt();
+		
+		System.out.println("\nREPORT:");
+		System.out.printf("Consumption = BRL %.2f%n",);
+		System.out.printf("Coverage = BRL %.2f%n",);
+		System.out.printf("Ticket = BRL %.2f%n",);
+		
+		System.out.printf("%nAmount payable = BRL %.2f%n",);
 		
 		sc.close();
 	}
